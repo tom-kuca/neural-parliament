8f7fb8ef5e7b89062fc9ee6ab69eb2f1cdd03800
Tomáš Kuča <tomas.kuca@matfyz.cz>
2011-01-07 17:08:58 +0100

    Opraven vypis z trenovani

--- ga.m
+++ ga.m
@@ -16,9 +16,9 @@ result = input(:, columnId)';
 % Vytvorit neuronovou sit
 
 net=newff(voting,result,[100],{},'traingdm');
-net.trainParam.lr = 0.01;
-net.trainParam.epochs = 100;
-net.trainParam.goal = 0.1;
+%net.trainParam.lr = 0.01;
+net.trainParam.epochs = 1000;
+net.trainParam.goal = 0.01;
 net.trainParam.max_fail = 20;
 
 % natrenovat neuronovou sit
@@ -26,15 +26,16 @@ net.trainParam.max_fail = 20;
 [trained_net,tr]=train(net,voting,result);
 simulation = hardlims(sim(trained_net, voting));
 
-% spocitact shodu s realnym hlasovanim v procentech
-%
-% pokud poslanec nehlasoval, pak se vysledek nezapocita
-miss = sum(simulation + result == 0);
-total = sum(result ~= 0);
-hits_pct = (total-miss) / total;
-
 % natrenovana sit se ulozi do soubor net.4.mat (4 je columnId)
 save trained_net;
 
+% spocitact shodu s realnym hlasovanim v procentech
+%
+% pokud poslanec nehlasoval, pak se vysledek nezapocita
+miss = sum(simulation + result == 0)
+total = sum(result ~= 0)
+hits_pct = (total-miss) / total;
+
+
 % vypsat shodu s realnym hlasovanim v procentech
-fprintf(1,'%d\n', hits_pct);
+fprintf(1,'%f\n', hits_pct);

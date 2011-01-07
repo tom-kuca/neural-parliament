995f018f00a6c71a5c4ed8341afb9b424377c0a0
Martin Majlis <martin@majlis.cz>
2011-01-07 12:43:36 +0100

    node.sh je nyni schopen spustit matlab i na PC bez X-serveru.

--- node.sh
+++ node.sh
@@ -12,7 +12,11 @@ fName=ga_$columnId;
 sed -r "s/^columnId = [0-9]+/columnId = $columnId/" ga.m > $fName.m;
 sed -ri "s/^function ga/function $fName/" $fName.m;
 
+export DISPLAY=:${columnId}
+Xvfb :${columnId} -screen 0 1024x768x16 &
+xb=`echo $!`;
 echo "quit; " | /afs/ms/@sys/bin/matlab -nodesktop -nosplash -r $fName > res.$columnId.complete;
+kill $xb &> /dev/null;
 tail -n2 res.$columnId.complete | head -n1 > res.$columnId;
 echo $columnId;
 cat res.$columnId;

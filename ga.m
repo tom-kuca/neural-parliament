ab425954f172ae2bc93a10cc5af1eb547c305092
Tomáš Kuča <tomas.kuca@matfyz.cz>
2011-01-07 11:57:37 +0100

    Prvni verze krabicky simulujici poslance

--- ga.m
+++ ga.m
@@ -1,13 +1,33 @@
-function ga(  )
+%function ga(  )
 
-columnId = 1;
+columnId = 19;
 
 % nejaky vypocet
+load input.txt
+
+num_votings = length(input);
+num_voters = length(input(1,:));
+
+voting = input(:, 1:columnId-1)';
+result = input(:, columnId)';
+
+net=newff(voting,result,[100],{},'traingdm');
+net.trainParam.lr = 0.01;
+net.trainParam.epochs = 1000;
+net.trainParam.goal = 0.1;
+net.trainParam.max_fail = 20;
+
+[net1,tr]=train(net,voting,result);
+simulation = hardlims(sim(net1, voting));
+
+miss = sum(simulation + result == 0)
+total = sum(result ~= 0)
+hits_pct = (total-miss) / total;
 
 % nekam se ulozi natrenovana neuronoa sit
 
-% shoda s poslancem
-disp '0.985';
+% shoda site s poslancem
+hits_pct
 
-end
+%end
 

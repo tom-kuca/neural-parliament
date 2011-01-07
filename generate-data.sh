c050408b8ba17fcdb9c7bdf7f8722781fe24bde1
Martin Majlis <martin@majlis.cz>
2011-01-07 14:42:48 +0100

    Pridany skripty na generovani testovacich dat.

---
+++ generate-data.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ $# != 2 ]; then 
+	echo "Usage: $0 members votings" >&2;
+	echo "$0 20 120" >&2;
+	echo "20 people are voting 120 times." >&2;
+	exit 2;
+fi;
+
+./generate-votes.pl $1 $2 > input.txt;
+./generate-members.pl $1 > members.txt;
---
+++ generate-members.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use utf8;
+
+=head1 INFO
+generuje jmena.
+
+=cut
+
+if ( $#ARGV != 0 ) { 
+	print STDERR "Usage: $0 members\n";
+	print STDERR "$0 20\n";
+	print STDERR "generates 20 people\n";
+	exit(1);
+}
+
+my $members = $ARGV[0];
+
+
+my $part = int($members / 3);
+
+for my $m (0 .. ($members - 1)) {
+	print sprintf("P%03d ", $m + 1);
+	if ( $m > $part ) {
+		if ( $m % 4 == 0 ) { 
+			print "(inv $m)";
+		} elsif ( $m % 5 == 0 ) { 
+			print "(same " . ($m + 1 - $part) .")";
+		} elsif ( $m % 6 == 0 ) { 
+			print "(sum)";
+		} elsif ( $m % 7 == 0 ) {  
+			print "(most)";
+		} else { 
+			print "(R)";
+		}
+	} else { 		
+		print "(R)";
+	}	
+	print "\n";
+}
+

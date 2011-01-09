56f96031277747d7ef50664e2445ecfc3a0b95cd
Martin Majlis <martin@majlis.cz>
2011-01-09 12:29:09 +0100

    U analyzy je mozne omezit analyzovane obdobi.

--- analyze-output.sh
+++ analyze-output.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
+
+# Usage: ./analyze-output.sh from to
+# Example: 
+# ./analyze-output.sh 10 11
+# prints just results from 10th turn.
+
+from=1;
+to=250;
+if [ x$1 != x ]; then
+	from=$1;
+fi;
+
+if [ x$2 != x ]; then
+	to=$2;
+fi;
+
 function compute
 {
-	cat /tmp/out.$$ | grep '\sP\s' | cut -f$1 | perl -e '@a=(); while (<STDIN> ) { chomp; push(@a, $_) ; } print "summary(c(", join(",", @a), "))";' | R --no-save | tail -n3 | head -n2
+	cat /tmp/out.$$ |  perl -ne " print if ( /^${from}\s/ .. /^${to}\s/ ) ; " | grep '\sP\s' | cut -f$1 | perl -e '@a=(); while (<STDIN> ) { chomp; push(@a, $_) ; } print "summary(c(", join(",", @a), "))";' | R --no-save | tail -n3 | head -n2
 
 }
 

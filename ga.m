83e7282f45d4adf6cd8582e37e51aeea4e7b2e54
Tomáš Kuča <tomas.kuca@matfyz.cz>
2011-01-10 19:10:04 +0100

    kontrola vstupu

--- ga.m
+++ ga.m
@@ -4,21 +4,30 @@ columnId = 1;
 % Nacist vstupni data
 load input.txt
 
-num_votings = length(input);
-num_voters = length(input(1,:));
+num_votings = length(input)
+num_voters = length(input(1,:))
+
+if columnId > num_voters
+    throw(MException('InputChk:OutOfRange', 'Column %d is out of range (%d).', columnId, num_voters))
+end
+
+if num_voters == 0 or num_votings == 0
+    throw(MException('InputChk:ErrInputFile', 'Input file seems to be empty.'))
+end
+
 
 % Ze vstupnich dat se vytvori 
 %       trenovaci data - odebere se sloupec columnId
 %       spravne vysledky - sloupec ColumnId
 voting = [input(:, 1:columnId-1) input(:, (columnId+1):num_voters)]';
-result = input(:, columnId)'
+result = input(:, columnId)';
 
 % Vytvorit neuronovou sit
-net=newff(voting,result,[20],{},'trainrp');
+net=newff(voting,result,[10],{},'trainscg');
 
 net.trainParam.epochs = 1000;
 net.trainParam.goal = 0.001;
-net.trainParam.max_fail = 20;
+net.trainParam.max_fail = 10;
 
 % natrenovat neuronovou sit
 %
